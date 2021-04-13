b67ae89005f7f4385554bf14aac395b89fec432d
al4str <al4str.ftw@gmail.com>
2021-04-13 18:24:50 +0300

    update run script

--- deploy/run.sh
+++ deploy/run.sh
@@ -2,4 +2,4 @@
 
 git pull origin master
 docker build -f /var/www/budget-api/deploy/Dockerfile -t "budget-api-image"
-docker run -p 4242:8080 -u "node" --memory-swap "1G" --mount source=budget-api-volume,target=/usr/src/app/db --name "budget-api" -d "budget-api-image"
+docker run -p 4242:8080 -u "node" -m "300M" --memory-swap "1G" --mount source=budget-api-volume,target=/usr/src/app/db --name "budget-api" -d "budget-api-image"
