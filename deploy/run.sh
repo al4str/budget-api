e59f7acbfac972e3fcb80bfbcd4712f8141e6661
al4str <al4str.ftw@gmail.com>
2021-04-13 19:53:54 +0300

    fix docker container already in use

--- deploy/run.sh
+++ deploy/run.sh
@@ -2,5 +2,7 @@
 
 cd /var/www/budget-api
 git pull origin master
-docker build -t "budget-api-image" -f ./deploy/Dockerfile .
-docker run --name "budget-api" -p 4242:8080 -v budget-api-volume:/usr/src/app/db -d "budget-api-image"
+docker stop budget-api
+docker rm budget-api
+docker build -t budget-api-image -f ./deploy/Dockerfile .
+docker run --name budget-api -p 4242:8080 -v budget-api-volume:/usr/src/app/db -d budget-api-image
