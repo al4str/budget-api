dbc553a8467f30b9da426d315193b8233ea01a1e
al4str <al4str.ftw@gmail.com>
2021-04-13 18:46:25 +0300

    simplify dockerfile even more

--- deploy/run.sh
+++ deploy/run.sh
@@ -3,4 +3,4 @@
 cd /var/www/budget-api
 git pull origin master
 docker build -t "budget-api-image" -f ./deploy/Dockerfile .
-docker run -p 4242:8080 -u "node" -m "300M" --memory-swap "1G" --mount source=budget-api-volume,target=/usr/src/app/db --name "budget-api" -d "budget-api-image"
+docker run --name "budget-api" -p 4242:8080 -v budget-api-volume:/usr/src/app/db -d "budget-api-image"
